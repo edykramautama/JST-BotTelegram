1c9568b777fff8fb24b2370f33dfe0064ef36393
Edy Krama Utama <70458618+eykramautama@users.noreply.github.com>
2021-05-29 00:12:08 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,11 +32,10 @@ bot.onText(/\/predict/, (msg) => {
 
 bot.on('message', (msg) => { 
     if(state == 1){
-        console.log(msg.text);
         s = msg.text.split("|");
         i = s[0]
         v = s[1]
-        model.predict{
+        model.predict(
             [
                 parseFloat(s[0]),
                 parseFloat(s[1])
