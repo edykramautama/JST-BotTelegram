a8f5c19cbb6c2e3ec4eae174c5d1846deaeed9af
Edy Krama Utama <70458618+eykramautama@users.noreply.github.com>
2021-05-28 22:25:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1855816107:AAEQSd42C0VCw8Q2SVAm7bB1yZJ3dq6Yrwo'
 const bot = new TelegramBot(token, {polling: true});
 
 
