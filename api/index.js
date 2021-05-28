b8fc1fce611ac99078b61d410f4b1d868c99234d
Edy Krama Utama <70458618+eykramautama@users.noreply.github.com>
2021-05-29 00:03:26 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -30,8 +30,9 @@ bot.onText(/\/predict/, (msg) => {
     state = 1;
 });
 
-bot.on('message', (msg.text) => { 
+bot.on('message', (msg) => { 
     if(state == 1){
+        console.log(msg.text)
         s = msg.text.split("|")
         i = s[0]
         v = s[1]
