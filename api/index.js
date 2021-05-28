16551f503a37e11b6d4fd422f67408587883b3b9
Edy Krama Utama <70458618+eykramautama@users.noreply.github.com>
2021-05-28 23:32:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,40 @@ bot.onText(/\/start/, (msg) => {
     );   
 });
 
+// input i and r
+state = 0;
+bot.onText(/\/predict/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `masukan nilai i|v contoh 9|9`
+    );   
+    state = 1;
+});
 
+bot.on('message', (msg) => { 
+    if(state == 1){
+        s = msg.text.split("|")
+        i = s[0]
+        v = s[1]
+        model.predict{
+            [
+                parseFloat(s[0]),
+                parseFloat(s[1])
+            ]
+         }.then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah $(jres[0]) volt`
+            );
+             bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah $(jres[a]) watt`
+            );
+        })
+     }else{
+         state = 0
+     }
+})
     
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
